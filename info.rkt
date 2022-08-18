aa3fdbcd650cdb8d0cb9495a0fe1b02f74d97832
JCPedroza <nemesis4go10@hotmail.com>
2022-08-18 09:30:13 -0500

    raco: add rackhunit dependency

--- info.rkt
+++ info.rkt
@@ -1,8 +1,8 @@
 #lang info
 (define collection "algorithms-and-data-structures")
-(define deps '("base"))
+(define deps '("base" "rackunit-lib"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/algorithms-and-data-structures.scrbl" ())))
-(define pkg-desc "Description Here")
+(define pkg-desc "Algorithms and data structures implemented in Racket")
 (define version "0.0")
-(define pkg-authors '(piubu))
+(define pkg-authors '(JCPedroza))
