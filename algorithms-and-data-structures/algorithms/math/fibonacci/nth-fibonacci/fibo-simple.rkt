f154c862ad1edb3eba62b4b6d0ee94b660b41d31
JCPedroza <nemesis4go10@hotmail.com>
2022-08-18 12:43:03 -0500

    refa: use variable indent style

--- algorithms-and-data-structures/algorithms/math/fibonacci/nth-fibonacci/fibo-simple.rkt
+++ algorithms-and-data-structures/algorithms/math/fibonacci/nth-fibonacci/fibo-simple.rkt
@@ -1,14 +1,13 @@
 #lang typed/racket
 
+(provide fibo)
+
+; Compute the nth Fibonacci number using simple recursion.
 (: fibo (-> Integer Integer))
 (define (fibo n)
-  (if
-    (< n 2)
-    n
-    (+
-      (fibo (sub1 n))
-      (fibo (- n 2)))))
-
-(provide fibo)
+  (if (< n 2)
+      n
+      (+ (fibo (sub1 n))
+         (fibo (- n 2)))))
 
 (fibo 6)
--- algorithms-and-data-structures/algorithms/math/fibonacci/nth-fibonacci/fibo-tail-call.rkt
+++ algorithms-and-data-structures/algorithms/math/fibonacci/nth-fibonacci/fibo-tail-call.rkt
@@ -1,13 +1,13 @@
 #lang typed/racket
 
+(provide fibo)
+
+; Compute the nth Fibonacci number using tail call recursion.
 (: fibo (-> Integer Integer))
 (define (fibo n)
   (let loop ((now 0) (nxt 1) (idx 0))
-    (if
-      (= n idx)
-      now
-      (loop nxt (+ now nxt) (add1 idx)))))
-
-(provide fibo)
+    (if (= n idx)
+        now
+        (loop nxt (+ now nxt) (add1 idx)))))
 
 (fibo 6)
