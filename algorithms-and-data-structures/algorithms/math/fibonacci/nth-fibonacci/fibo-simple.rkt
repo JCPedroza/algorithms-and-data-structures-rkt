0a51fa33cb3489fb06b406c597e145dbbfeaeb72
JCPedroza <nemesis4go10@hotmail.com>
2022-08-18 08:47:32 -0500

    feat (algo): implement recursive nth-fibonacci

---
+++ algorithms-and-data-structures/algorithms/math/fibonacci/nth-fibonacci/fibo-simple.rkt
@@ -0,0 +1,7 @@
+#lang racket/base
+
+(define (fibo n)
+  (if
+    (< n 2)
+    n
+    (+ (fibo (sub1 n)) (fibo (- n 2)))))
---
+++ algorithms-and-data-structures/algorithms/math/fibonacci/nth-fibonacci/fibo-tail-call.rkt
@@ -0,0 +1,8 @@
+#lang racket/base
+
+(define (fibo n)
+  (let loop ((now 0) (nxt 1) (idx 0))
+    (if
+      (= n idx)
+      now
+      (loop nxt (+ now nxt) (add1 idx)))))
