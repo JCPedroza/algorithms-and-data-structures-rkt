c198ab0bb5fc913ff84ebc793fda870d8df540c1
JCPedroza <nemesis4go10@hotmail.com>
2022-08-18 12:45:45 -0500

    raco: declare math-lib dependency

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "algorithms-and-data-structures")
-(define deps '("base" "rackunit-lib" "typed-racket-lib"))
+(define deps '("base" "rackunit-lib" "typed-racket-lib" "math-lib"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/algorithms-and-data-structures.scrbl" ())))
 (define pkg-desc "Algorithms and data structures implemented in Racket")
