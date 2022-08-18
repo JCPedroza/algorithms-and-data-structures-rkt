8c1e2262b30f9c4115eef900912d8907a21f03b4
Carlos Pedroza <97198247+IngCarlosPedroza@users.noreply.github.com>
2022-08-18 11:36:34 -0500

    only-in test

--- algorithms-and-data-structures/algorithms/math/fibonacci/nth-fibonacci/test-fibo.rkt
+++ algorithms-and-data-structures/algorithms/math/fibonacci/nth-fibonacci/test-fibo.rkt
@@ -1,5 +1,9 @@
 #lang racket/base
 
-(require rackunit "fibo-simple.rkt")
+(require rackunit)
 
-(check-equal? (fibo 0) 0 "test")
+(requrie 
+  (only-in 
+    ["fibo-simple.rkt" fibo-simple]))
+
+(check-equal? (fibo-simple 0) 0 "test")
