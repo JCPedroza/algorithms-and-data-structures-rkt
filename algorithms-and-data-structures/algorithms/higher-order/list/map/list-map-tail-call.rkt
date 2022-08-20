e20b7995aaab3a37d29d6884c7e1fa1ef4eb38e1
JCPedroza <nemesis4go10@hotmail.com>
2022-08-20 08:31:47 -0500

    fix: reverse list order

--- algorithms-and-data-structures/algorithms/higher-order/list/map/list-map-tail-call.rkt
+++ algorithms-and-data-structures/algorithms/higher-order/list/map/list-map-tail-call.rkt
@@ -3,11 +3,11 @@
 (provide map)
 
 (define (map f xs)
-  (let loop ((g f) (ys xs) (map-list null))
+  (reverse (let loop ((ys xs) (maps null))
     (if (null? ys)
-        map-list
-        (loop g (cdr ys) (cons (g (car ys))
-                               map-list)))))
+        maps
+        (loop (cdr ys) (cons (f (car ys))
+                               maps))))))
 
 (define nums (list 0 1 2 3 4))
 
