8fab1fc3dd9e68b7b479980308ff210536137392
JCPedroza <nemesis4go10@hotmail.com>
2022-08-20 06:25:03 -0500

    feat (algo): implement for loop variant

---
+++ algorithms-and-data-structures/algorithms/math/fibonacci/nth-fibonacci/fibo-for.rkt
@@ -0,0 +1,15 @@
+#lang typed/racket
+
+; Calculate the nth Fibonacci number using a for loop and three variables.
+(: fibo (-> Integer Integer))
+(define (fibo n)
+  (let ([now 0]
+        [nxt 1]
+        [tmp 0])
+       (for ([idx n])
+         (set! tmp now)
+         (set! now nxt)
+         (set! nxt (+ nxt tmp)))
+       now))
+
+(println (fibo 6))
