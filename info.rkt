c9c3afb7018ffcc98722906841aa3882a4a56efb
JCPedroza <nemesis4go10@hotmail.com>
2022-08-21 05:16:08 -0500

    conf: put each dependency on its own line

--- info.rkt
+++ info.rkt
@@ -1,7 +1,17 @@
 #lang info
 (define collection "algorithms-and-data-structures")
-(define deps '("base" "rackunit-lib" "typed-racket-lib" "math-lib"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+
+(define deps '(
+  "base"
+  "rackunit-lib"
+   "typed-racket-lib"
+    "math-lib"))
+
+(define build-deps '(
+  "scribble-lib"
+  "racket-doc"
+   "rackunit-lib"))
+
 (define scribblings '(("scribblings/algorithms-and-data-structures.scrbl" ())))
 (define pkg-desc "Algorithms and data structures implemented in Racket")
 (define version "0.0")
