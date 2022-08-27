ebbe14884ccd0920aaf8502f9e875751b634cc76
JCPedroza <nemesis4go10@hotmail.com>
2022-08-27 14:39:20 -0500

    rafa: use typed racket

--- algorithms-and-data-structures/classic-interview/fizz-buzz/fizz-buzz-simple-selection.rkt
+++ algorithms-and-data-structures/classic-interview/fizz-buzz/fizz-buzz-simple-selection.rkt
@@ -1,7 +1,8 @@
-#lang racket/base
+#lang typed/racket
 
-; Convert number to fizz buzz string
-(define (num->fb num)
+; Convert number to fizz buzz string using string selection.
+(: integer->fizz-buzz (-> Integer String))
+(define (integer->fizz-buzz num)
   (cond
     [(zero? (modulo num 15)) "fizzbuzz"]
     [(zero? (modulo num 3)) "fizz"]
@@ -10,12 +11,13 @@
 
 ; Build list of fizz buzz sequence within the given range, using string
 ; selection and simple recursion.
+(: fizz-buzz (-> Integer Integer (Listof String)))
 (define (fizz-buzz from to)
   (if
     (> from to)
     null
     (cons
-      (num->fb from)
+      (integer->fizz-buzz from)
       (fizz-buzz (add1 from) to))))
 
 (provide fizz-buzz)
