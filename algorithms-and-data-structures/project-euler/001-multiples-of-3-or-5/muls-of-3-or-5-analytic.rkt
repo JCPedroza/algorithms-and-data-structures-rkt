8526667ca6574bb97830d06973ffb13d36456535
JCPedroza <nemesis4go10@hotmail.com>
2022-08-29 15:35:34 -0500

    refa: change name of helper function

--- algorithms-and-data-structures/project-euler/001-multiples-of-3-or-5/muls-of-3-or-5-analytic.rkt
+++ algorithms-and-data-structures/project-euler/001-multiples-of-3-or-5/muls-of-3-or-5-analytic.rkt
@@ -4,8 +4,8 @@
 ; 1 + 2 + 3 + ... +  n = n(n + 1) / 2
 ; 3 + 6 + 9 + ... + 3n = 3 (1 + 2 + 3 + ... + n)
 ; https://en.wikipedia.org/wiki/1_%2B_2_%2B_3_%2B_4_%2B_%E2%8B%AF
-(: mul-sum-of (-> Integer Integer Integer))
-(define (mul-sum-of num limit)
+(: mul-of-sum (-> Integer Integer Integer))
+(define (mul-of-sum num limit)
   (let ([ p (quotient (sub1 limit) num) ])
     (quotient (* num (* p (add1 p))) 2)))
 
@@ -14,7 +14,7 @@
 (: muls-of-3-or-5 (-> Integer Integer))
 (define (muls-of-3-or-5 limit)
   (-
-    (+ (mul-sum-of 3 limit) (mul-sum-of 5 limit))
-    (mul-sum-of 15 limit))) ; Don't count multiples of both twice
+    (+ (mul-of-sum 3 limit) (mul-of-sum 5 limit))
+    (mul-of-sum 15 limit))) ; Don't count multiples of both twice
 
 (provide muls-of-3-or-5)
