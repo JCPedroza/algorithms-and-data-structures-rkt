09d096fb69bd60c5dd2f07a7affe153b027cc2d0
JCPedroza <nemesis4go10@hotmail.com>
2022-08-29 15:20:41 -0500

    feat (euler 1): implement, test 'analytic' variant

---
+++ algorithms-and-data-structures/project-euler/001-multiples-of-3-or-5/muls-of-3-or-5-analytic.rkt
@@ -0,0 +1,20 @@
+#lang typed/racket
+
+; Compute sum of multiples of [num] that are less than [limit].
+; 1 + 2 + 3 + ... +  n = n(n + 1) / 2
+; 3 + 6 + 9 + ... + 3n = 3 (1 + 2 + 3 + ... + n)
+; https://en.wikipedia.org/wiki/1_%2B_2_%2B_3_%2B_4_%2B_%E2%8B%AF
+(: mul-sum-of (-> Integer Integer Integer))
+(define (mul-sum-of num limit)
+  (let ([ p (quotient (sub1 limit) num) ])
+    (quotient (* num (* p (add1 p))) 2)))
+
+; Compute sum of multiples of 3 or 5 that are less than [limit],
+; using an analytic approach.
+(: muls-of-3-or-5 (-> Integer Integer))
+(define (muls-of-3-or-5 limit)
+  (-
+    (+ (mul-sum-of 3 limit) (mul-sum-of 5 limit))
+    (mul-sum-of 15 limit))) ; Don't count multiples of both twice
+
+(provide muls-of-3-or-5)
