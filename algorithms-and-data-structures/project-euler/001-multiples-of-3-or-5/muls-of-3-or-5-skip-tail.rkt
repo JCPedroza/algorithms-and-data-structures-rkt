335b1355d1fc804113264c21c29254bbc83e3c43
JCPedroza <nemesis4go10@hotmail.com>
2022-08-29 13:55:05 -0500

    feat (euler 1): implement, test 'skip tail' variant

---
+++ algorithms-and-data-structures/project-euler/001-multiples-of-3-or-5/muls-of-3-or-5-skip-tail.rkt
@@ -0,0 +1,24 @@
+#lang typed/racket
+
+; Add five to integer only if it's positive (larger than zero), else returns
+; zero.
+(: add5 (-> Integer Integer))
+(define (add5 int) (if (> int 0) (+ int 5) 0))
+
+; Compute sum of multiples of 3 or 5 that are less than the specified limit,
+; using tail racursion and skipped sequences.
+(: muls-of-3-or-5 (-> Integer Integer))
+(define (muls-of-3-or-5 limit)
+  (let loop ((sum 0) (threes 3) (fives 5))
+    (cond
+      [(>= threes limit) sum]
+
+      [(>= fives limit)
+        (loop sum threes 0)] ; Fives adds zero when it reaches its limit
+
+      [(zero? (modulo fives 3)) ; Don't count multiples of both twice
+        (loop (+ sum threes) (+ threes 3) (add5 fives))]
+
+      [#t (loop (+ sum threes fives) (+ threes 3) (add5 fives))])))
+
+(provide muls-of-3-or-5)
--- algorithms-and-data-structures/project-euler/001-multiples-of-3-or-5/muls-of-3-or-5.repo.rkt
+++ algorithms-and-data-structures/project-euler/001-multiples-of-3-or-5/muls-of-3-or-5.repo.rkt
@@ -3,11 +3,13 @@
 (require
   (prefix-in brute-simple: "muls-of-3-or-5-brute-simple.rkt")
   (prefix-in brute-tail: "muls-of-3-or-5-brute-simple.rkt")
-  (prefix-in brute-for: "muls-of-3-or-5-brute-for.rkt"))
+  (prefix-in brute-for: "muls-of-3-or-5-brute-for.rkt")
+  (prefix-in skip-tail: "muls-of-3-or-5-skip-tail.rkt"))
 
 (define solutions (list
   brute-simple:muls-of-3-or-5
   brute-tail:muls-of-3-or-5
-  brute-for:muls-of-3-or-5))
+  brute-for:muls-of-3-or-5
+  skip-tail:muls-of-3-or-5))
 
 (provide solutions)
